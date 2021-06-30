bbed9990cc70c991bed8046af9a0fb48caad1d85
Kingsley Abru <kingsleyabru11@outlook.com>
2021-06-30 21:02:52 +0000

    Update SQL queries

--- sql_queries.py
+++ sql_queries.py
@@ -1,10 +1,10 @@
 # DROP TABLES
 
-songplay_table_drop = "DROP TABLE IF EXISTS songplays CASCADE"
-user_table_drop = "DROP TABLE IF EXISTS users CASCADE"
-song_table_drop = "DROP TABLE IF EXISTS songs CASCADE"
-artist_table_drop = "DROP TABLE IF EXISTS artists CASCADE"
-time_table_drop = "DROP TABLE IF EXISTS time CASCADE"
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
@@ -13,20 +13,12 @@ CREATE TABLE IF NOT EXISTS songplays(
     songplay_id SERIAL PRIMARY KEY,
     start_time TIMESTAMP NOT NULL,
     user_id INT NOT NULL,
-    level INT NOT NULL,
+    level VARCHAR(50) NOT NULL,
     song_id VARCHAR(50),
     artist_id VARCHAR(50),
     session_id INT NOT NULL,
     location VARCHAR(255) NOT NULL,
-    user_agent VARCHAR(255) NOT NULL,
-    FOREIGN KEY (start_time)
-        REFERENCES time (start_time),
-    FOREIGN KEY (user_id)
-        REFERENCES users (user_id),
-    FOREIGN KEY (song_id)
-        REFERENCES songs (song_id),
-    FOREIGN KEY (artist_id)
-        REFERENCES artists (artist_id)
+    user_agent VARCHAR(255) NOT NULL
 );
 """)
 
@@ -38,7 +30,7 @@ CREATE TABLE IF NOT EXISTS users(
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     gender current_gender NOT NULL,
-    level INT
+    level VARCHAR (50)
 );
 """)
 
@@ -48,9 +40,7 @@ CREATE TABLE IF NOT EXISTS songs(
     title VARCHAR(255),
     artist_id VARCHAR(50),
     year INT,
-    duration NUMERIC,
-    FOREIGN KEY(artist_id)
-        REFERENCES artists (artist_id)
+    duration NUMERIC
 );
 """)
 
@@ -79,27 +69,45 @@ CREATE TABLE IF NOT EXISTS time(
 # INSERT RECORDS
 
 songplay_table_insert = ("""
+    INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+    ON CONFLICT DO NOTHING;
 """)
 
 user_table_insert = ("""
+    INSERT INTO users (user_id, first_name, last_name, gender, level)
+    VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT DO NOTHING;
 """)
 
 song_table_insert = ("""
+    INSERT INTO songs (song_id, title, artist_id, year, duration)
+    VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT DO NOTHING;
 """)
 
 artist_table_insert = ("""
+    INSERT INTO artists (artist_id, name, location, latitude, longitude)
+    VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT DO NOTHING;
 """)
 
 
 time_table_insert = ("""
+    INSERT INTO time (start_time, hour, day, week, month, year, weekday)
+    VALUES (%s, %s, %s, %s, %s, %s, %s)
+    ON CONFLICT DO NOTHING;
 """)
 
 # FIND SONGS
 
 song_select = ("""
+    SELECT songs.song_id, artists.artist_id FROM songs
+    JOIN artists ON songs.artist_id = artists.artist_id
+    WHERE songs.title = %s AND artists.name = %s AND songs.duration = %s;
 """)
 
 # QUERY LISTS
 
-create_table_queries = [artist_table_create, user_table_create, song_table_create, time_table_create, songplay_table_create]
+create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
 drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
