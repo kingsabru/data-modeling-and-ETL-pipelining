a20a4d0e1519483a29b2c4f74b62d5efb39852e9
Kingsley Abru <kingsleyabru11@outlook.com>
2021-06-30 21:55:39 +0000

    Implement ETL in Python

--- etl.py
+++ etl.py
@@ -1,5 +1,6 @@
 import os
 import glob
+from decouple import config
 import psycopg2
 import pandas as pd
 from sql_queries import *
@@ -7,58 +8,86 @@ from sql_queries import *
 
 def process_song_file(cur, filepath):
     # open song file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
-    # insert song record
-    song_data = 
-    cur.execute(song_table_insert, song_data)
-    
-    # insert artist record
-    artist_data = 
-    cur.execute(artist_table_insert, artist_data)
+    for index, row in df.iterrows():
+        # insert song record
+        song_data = (row.song_id, row.title, row.artist_id, row.year, row.duration)
+        
+        try:
+            cur.execute(song_table_insert, song_data)
+        except psycopg2.Error as error:
+            print("Error: Inserting row for table 'song'")
+            print(error)
+
+        # insert artist record
+        artist_data = (row.artist_id, row.artist_name, row.artist_location, row.artist_latitude, row.artist_longitude)
+        try:
+            cur.execute(artist_table_insert, artist_data)
+        except psycopg2.Error as error:
+            print("Error: Inserting row for table 'artists'")
+            print (error)
 
 
 def process_log_file(cur, filepath):
     # open log file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
     # filter by NextSong action
-    df = 
+    df = df[df.page == 'NextSong']
 
     # convert timestamp column to datetime
-    t = 
+    df['ts'] = pd.to_datetime(df['ts'], unit='ms')
+    t = df.copy()
     
     # insert time data records
-    time_data = 
-    column_labels = 
-    time_df = 
+    time_data = (t.ts, t.ts.dt.hour , t.ts.dt.day , t.ts.dt.dayofweek , t.ts.dt.month , t.ts.dt.year , t.ts.dt.weekday)
+    column_labels = ['start_time', 'hour', 'day', 'week', 'month', 'year', 'weekday']
+    time_df = pd.DataFrame(columns=column_labels)
+
+    for index, column_label in enumerate(column_labels):
+        time_df[column_label] = time_data[index]
 
     for i, row in time_df.iterrows():
-        cur.execute(time_table_insert, list(row))
+        try:
+            cur.execute(time_table_insert, list(row))
+        except psycopg2.Error as error:
+            print("Error: Inserting row for table 'time'")
+            print (error)
 
     # load user table
-    user_df = 
+    user_df = df[['userId', 'firstName', 'lastName', 'gender', 'level']]
 
     # insert user records
     for i, row in user_df.iterrows():
-        cur.execute(user_table_insert, row)
+        try:
+            cur.execute(user_table_insert, row)
+        except psycopg2.Error as error:
+            print("Error: Inserting row for table 'users'")
+            print (error)
 
     # insert songplay records
     for index, row in df.iterrows():
-        
-        # get songid and artistid from song and artist tables
-        cur.execute(song_select, (row.song, row.artist, row.length))
-        results = cur.fetchone()
-        
-        if results:
-            songid, artistid = results
-        else:
-            songid, artistid = None, None
+        try:
+            # get songid and artistid from song and artist tables
+            cur.execute(song_select, (row.song, row.artist, row.length))
+            results = cur.fetchone()
+            
+            if results:
+                songid, artistid = results
+            else:
+                songid, artistid = None, None
 
-        # insert songplay record
-        songplay_data = 
-        cur.execute(songplay_table_insert, songplay_data)
-
+            # insert songplay record
+            songplay_data = (row.ts, row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent)
+            try:
+                cur.execute(songplay_table_insert, songplay_data)
+            except psycopg2.Error as error:
+                print("Error: Inserting row for table 'songplays'")
+                print (error)
+        except psycopg2.Error as error:
+            print("Error: Querying for Song ID and Artist ID")
+            print (error)
 
 def process_data(cur, conn, filepath, func):
     # get all files matching extension from directory
@@ -80,8 +109,12 @@ def process_data(cur, conn, filepath, func):
 
 
 def main():
-    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    user = config('DB-USER')
+    password = config('DB-PASSWORD')
+
+    conn = psycopg2.connect("dbname=sparkifydb user={} password={}".format(user, password))
     cur = conn.cursor()
+
 
     process_data(cur, conn, filepath='data/song_data', func=process_song_file)
     process_data(cur, conn, filepath='data/log_data', func=process_log_file)
